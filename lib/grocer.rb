d7bb46419433868fb17e072ae8baa4a2672d7011
James B Schoen <edensongjbs@gmail.com>
2020-05-19 21:41:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -46,4 +46,17 @@ def consolidate_cart(cart)
   #end
 end
 
-consolidate_cart([])
+CART=[
+  {:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "KALE", :price => 3.00, :clearance => false},
+  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+  {:item => "ALMONDS", :price => 9.00, :clearance => false},
+  {:item => "TEMPEH", :price => 3.00, :clearance => true},
+  {:item => "CHEESE", :price => 6.50, :clearance => false},
+  {:item => "BEER", :price => 13.00, :clearance => false},
+  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+  {:item => "BEETS", :price => 2.50, :clearance => false},
+  {:item => "SOY MILK", :price => 4.50, :clearance => true}
+]
+
+consolidate_cart(CART)
