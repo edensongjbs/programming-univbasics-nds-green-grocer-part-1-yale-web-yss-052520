d363e75e4bd81c394d80fc0c5a693a400c741393
James B Schoen <edensongjbs@gmail.com>
2020-05-19 19:48:13 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,8 +16,8 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   cart.reduce do |consolidated_cart=[], item_hash|
     item_name=item_hash[:item]
-    item_found?=find_item_by_name_in_collection(item_name, cart)
-    if item_found? do
+    item_found=find_item_by_name_in_collection(item_name, cart)
+    if item_found do
       consolidated_cart = consolidated_cart.map do |x|
         if x[:item]==item_name
           x[:count]+=1
